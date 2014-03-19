f1fd83804f144d01ec0602f6dbdb74b892b74e8c
Rob <robianmcd@gmail.com>
2014-03-19 19:55:29 -0400

    Added search terms to a query parameter

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
 var app = angular.module('angularDocTalk', ['ui.bootstrap', 'firebase']);
 
-var mainCtrl = function($sce, $firebaseSimpleLogin, $firebase) {
+var MainCtrl = function($sce, $firebaseSimpleLogin, $firebase, $location) {
     var _this = this;
 
     this.$sce = $sce;
+    this.$location = $location;
 
     this.dbRef = new Firebase('https://angular-doc-talk.firebaseio.com');
     this.auth = $firebaseSimpleLogin(this.dbRef);
 
-    this.comments = $firebase(this.dbRef.child('comments'));
+    this.commentsDb = $firebase(this.dbRef.child('comments'));
 
-    this.searchTerms = [];
-    this.docInfoByTerm = {};
+    this.docTitles = [];
+    this.docInfoByTitle = {};
 
     for (key in NG_PAGES) {
         //noinspection JSUnfilteredForInLoop
         var curDocInfo = NG_PAGES[key];
         if (curDocInfo.searchTerms && curDocInfo.searchTerms.titleWords) {
-            this.searchTerms.push(curDocInfo.searchTerms.titleWords);
-            this.docInfoByTerm[curDocInfo.searchTerms.titleWords] = curDocInfo;
+            var title;
+
+            //If there already exists documentation with the same title word then use the path as the search term.
+            if (this.docInfoByTitle[curDocInfo.searchTerms.titleWords]) {
+                title = curDocInfo.path;
+            }
+            else {
+                title = curDocInfo.searchTerms.titleWords;
+            }
+
+            this.docTitles.push(title);
+            curDocInfo.title = title;
+            this.docInfoByTitle[title] = curDocInfo;
         }
+    }
+
+    //If there is a search term in the url then search for it
+    if (this.$location.search().search) {
+        this.userSearch = this.$location.search().search;
+        this.search();
     }
 };
 
-mainCtrl.prototype.search = function() {
-    if (this.docInfoByTerm[this.userSearchTerm]) {
-        this.selectedDocInfo = this.docInfoByTerm[this.userSearchTerm];
+MainCtrl.prototype.search = function() {
+    if (this.docInfoByTitle[this.userSearch]) {
+        this.selectedDocInfo = this.docInfoByTitle[this.userSearch];
         this.docUrl = this.$sce.trustAsResourceUrl("http://code.angularjs.org/1.2.14/docs/" + this.selectedDocInfo.outputPath);
+
+        this.$location.search('search', this.selectedDocInfo.title);
     }
     else {
         this.docUrl = "";
     }
 };
 
-mainCtrl.prototype.loginWithGoogle = function() {
+MainCtrl.prototype.loginWithGoogle = function() {
     this.auth.$login('google');
 };
 
-mainCtrl.prototype.logout = function() {
+MainCtrl.prototype.logout = function() {
     this.auth.$logout();
 };
 
-mainCtrl.prototype.postComment = function() {
-    this.comments.$add({comment: this.userComment});
+MainCtrl.prototype.postComment = function() {
+    this.commentsDb.$add({
+        comment: this.userComment,
+        poster: {
+            pic: this.auth.user.thirdPartyUserData.picture,
+            name: this.auth.user.displayName,
+            uid: this.auth.user.uid
+        }
+    });
+
+    this.userComment = "";
 };
