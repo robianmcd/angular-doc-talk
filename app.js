945382cb4c43ef113fdd5f92af80e9b5a0bb6f8f
Rob <robianmcd@gmail.com>
2014-03-18 23:24:18 -0400

    Added google authentication

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
-var app = angular.module('angularDocTalk', ['ui.bootstrap']);
+var app = angular.module('angularDocTalk', ['ui.bootstrap', 'firebase']);
 
-var mainCtrl = function($sce) {
+var mainCtrl = function($sce, $firebaseSimpleLogin, $firebase) {
+    var _this = this;
+
     this.$sce = $sce;
 
+    this.dbRef = new Firebase('https://angular-doc-talk.firebaseio.com');
+    this.auth = $firebaseSimpleLogin(this.dbRef);
+
+    this.comments = $firebase(this.dbRef.child('comments'));
+
     this.searchTerms = [];
-    this.docInfo = {};
+    this.docInfoByTerm = {};
 
     for (key in NG_PAGES) {
         //noinspection JSUnfilteredForInLoop
         var curDocInfo = NG_PAGES[key];
         if (curDocInfo.searchTerms && curDocInfo.searchTerms.titleWords) {
             this.searchTerms.push(curDocInfo.searchTerms.titleWords);
-            this.docInfo[curDocInfo.searchTerms.titleWords] = curDocInfo;
+            this.docInfoByTerm[curDocInfo.searchTerms.titleWords] = curDocInfo;
         }
     }
 };
 
 mainCtrl.prototype.search = function() {
-    if (this.docInfo[this.userSearchTerm]) {
-        this.selectedDocInfo = this.docInfo[this.userSearchTerm];
+    if (this.docInfoByTerm[this.userSearchTerm]) {
+        this.selectedDocInfo = this.docInfoByTerm[this.userSearchTerm];
         this.docUrl = this.$sce.trustAsResourceUrl("http://code.angularjs.org/1.2.14/docs/" + this.selectedDocInfo.outputPath);
     }
+    else {
+        this.docUrl = "";
+    }
+};
+
+mainCtrl.prototype.loginWithGoogle = function() {
+    this.auth.$login('google');
+};
+
+mainCtrl.prototype.logout = function() {
+    this.auth.$logout();
+};
+
+mainCtrl.prototype.postComment = function() {
+    this.comments.$add({comment: this.userComment});
 };
